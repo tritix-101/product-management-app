f834786c9114b9f1d76a2a12ac87c16a2d2b8baa
Priyanka Kandula <priyankakandula2004@gmail.com>
2025-09-09 16:38:19 +0530

    Added Product schema with basic validations

---
+++ backend/src/models/Product.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 100,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    description: {
+      type: String,
+      default: "Usable Product",
+      maxlength: 500,
+    },
+    category: {
+      type: String,
+      required: true,
+      default: "General",
+    },
+  },
+  { timestamps: true } 
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
